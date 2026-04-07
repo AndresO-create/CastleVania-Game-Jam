5f3be4f431783b23770def8d008e4360bcaabaa2
Andres Ortiz <andresortiz1212345@gmail.com>
2026-04-07 11:44:02 -0400

    Reorganized

---
+++ castlevania-game-jam/Scripts/Levels/level.gd
@@ -0,0 +1,31 @@
+extends Node2D
+class_name Level
+@onready var camera: Camera2D = $Camera
+@onready var player_spawn: Marker2D = $PlayerSpawn
+@onready var player : Player = $"../Player"
+@onready var left_bound: CollisionShape2D = $Boundaries/LeftBound
+@onready var right_bound: CollisionShape2D = $Boundaries/RightBound
+@onready var boss_arena: Area2D = $BossArena
+
+
+func _process(delta: float) -> void:
+	camera.position.x = player.position.x
+
+func _on_kill_plane_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.health -= body.health
+
+
+func _on_level_transition_body_entered(body: Node2D) -> void:
+	get_parent().call_deferred("next_level")
+	call_deferred("queue_free")
+	GameManager.transition.play()
+
+
+#reset level bounds to accomodate for bossfight 
+func _on_boss_arena_body_entered(body: Node2D) -> void:
+	camera.limit_left = boss_arena.position.x - 128.0
+	left_bound.position.x = boss_arena.position.x - 128.0
+	var boss : Boss = $Tilemap/Boss
+	boss.animation_player.play("Wake")
+	$BossArena.queue_free()
